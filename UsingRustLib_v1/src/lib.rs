a8812bd7ad81e256bb981aea0fd794fe692d2643
randymcmillan <randymcmillan@protonmail.com>
2026-01-04 13:32:46 -0500

    UsingRustLib_v1/src/lib.rs:add fGnostrVersion

--- UsingRustLib_v1/src/lib.rs
+++ UsingRustLib_v1/src/lib.rs
@@ -3,6 +3,7 @@
 
 use std::ffi::CString;
 use std::os::raw::c_char;
+use gnostr::version;
 
 #[no_mangle]
 pub extern "C" fn fStringFromRustLibrary() -> *const c_char {
@@ -15,4 +16,11 @@ pub extern "C" fn fStartRelay() -> *const c_char {
 
 	let oOutput: CString = CString::new("fStartRelay").unwrap();
 	return oOutput.into_raw();
+}
+
+#[no_mangle]
+pub extern "C" fn fGnostrVersion() -> *const c_char {
+    let version_string = version();
+    let c_string = CString::new(version_string).unwrap();
+    c_string.into_raw()
 }
